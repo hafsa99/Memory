c64b6ac27185a955219768869cf40a366992f42f
AKucinicA <35217885+AKucinicA@users.noreply.github.com>
2019-05-23 11:54:00 +0200

    Update memory.c

--- memory.c
+++ memory.c
@@ -4,28 +4,45 @@
 //#include "cartes.h"
 
 typedef struct{
-	Widget bouton;
 	int valeur;
 	int etat;
 } Carte;
 
-int main(int argc, char const *argv[])
-{
-	Carte c1;
-	Carte c2;
-	Carte c3;
-	Carte c4;
+void retourner(Carte *c){
+	c->etat=!(c->etat);
+}
 
-	c1.valeur = 0;
-	c2.valeur = 0;
-	c3.valeur = 1;
-	c4.valeur = 1;
+void initCarte(Carte *c, int val){
+	c->valeur = val;
+	c->etat = 0;
+}
 
-	c1.etat = 0;
-	c2.etat = 0;
-	c3.etat = 0;
-	c4.etat = 0;
+int getVal(Carte c){
+	return c.valeur;
+}
 
+int getEtat(Carte c){
+	return c.etat;
+}
+
+void initJeu(Carte j[], int nbPaires){
+	int i = 0;
+	while(i++ < 2*nbPaires + 1){
+		initCarte(&j[i],i%nbPaires);
+	}
+
+}
+
+int main(int argc, char const *argv[]){
+
+	int nbPaires=3;
+
+	Carte jeu[2*nbPaires + 1];
+	initJeu(jeu,nbPaires);
+
+	for(int i=0; i<2*nbPaires; i++){
+		retourner(&jeu[i]);
+	}
 
 	return EXIT_SUCCESS;
 }
